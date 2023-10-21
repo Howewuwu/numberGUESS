429a368d5144911ad5c191fc31278ba40e50a554
Howe <howewu1991@gmail.com>
2023-10-22 01:47:31 +0800

    AutoLayout finish, code 5 %

---
+++ numberGUESS/oneAtwoBViewController.swift
@@ -0,0 +1,29 @@
+//
+//  oneAtwoBViewController.swift
+//  numberGUESS
+//
+//  Created by Howe on 2023/10/22.
+//
+
+import UIKit
+
+class oneAtwoBViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
